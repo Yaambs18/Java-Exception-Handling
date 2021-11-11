2469355393a8cdc6b1f8d7d4d49159418c304e0e
yaambs <yanshbhardwaj14@gmail.com>
2021-11-11 09:59:20 +0530

    method nested try block Exception handling example code added

---
+++ src/com/exceptionhandling/MethodNestedTry.java
@@ -0,0 +1,31 @@
+package com.exceptionhandling;
+
+public class MethodNestedTry {
+    static void nestTry(int a){
+        try{
+            if(a==1){
+                a=a/(a-a);
+            }
+            if(a==2) {
+                int c[] = {1};
+                c[42] = 99;
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Array index oob: "+e);
+        }
+    }
+
+    public static void main(String[] args) {
+        try{
+            int a = args.length;;
+            int b =42/a;
+            System.out.println("a + "+a);
+
+            nestTry(a);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Divide by 0: "+e);
+        }
+    }
+}
